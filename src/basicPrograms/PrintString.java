75c5ebac25bee106e7014ba2b6e6682de2cc3357
shivarepository <shiva.swami88@gmail.com>
2023-08-11 20:21:05 +0530

    Update commit

---
+++ src/basicPrograms/PrintString.java
@@ -0,0 +1,16 @@
+package basicPrograms;
+
+import java.util.Scanner;
+
+public class PrintString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		System.out.println(str);
+
+	}
+
+}
