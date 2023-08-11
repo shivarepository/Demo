90bba96404f9e92dbb395b914caa7c0e284d82b1
shivarepository <shiva.swami88@gmail.com>
2023-08-11 17:50:47 +0530

    Commited from Demo project

---
+++ src/basicPrograms/Demo.java
@@ -0,0 +1,11 @@
+package basicPrograms;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Java very interesting programming language");
+	}
+
+}
